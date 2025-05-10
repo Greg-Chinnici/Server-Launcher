85ac58784bfcbc8754c7b52322b9dd68ce7b9f49
greg-chinnici <chinnici@chapman.edu>
2025-05-10 01:05:09 -0700

    server lauch func

--- Server-Launcher/src/main.rs
+++ Server-Launcher/src/main.rs
@@ -3,6 +3,6 @@ mod servers;
 
 fn main() {
     println!("Hello, world!");
-
+    println!("Current OS: {}", std::env::consts::OS);
     tui::init_tui();
 }
--- Server-Launcher/src/servers.rs
+++ Server-Launcher/src/servers.rs
@@ -1,9 +1,35 @@
+use std::{error::Error, fmt::format};
+use std::process::{Command, Stdio};
 
-
+#[derive(Debug, Clone)]
 struct Server {
-    name: String,
-    game: String,
-    path: String,
-    args: String,
-    auto_start: bool
+    pub name: String,
+    pub path: String,
+    pub executable: String,
+    pub args: Vec<String>,
+    pub autostart: bool,
+}
+
+pub fn Launch(server: Server) {
+    let (shell, shell_flag, change_dir) = match std::env::consts::OS {
+        "windows" => ("cmd", "/C", format!("cd /d {}", server.path)),
+        "macos" | "linux" => ("sh", "-c", format!("cd '{}' &&", server.path)),
+        _ => {
+            eprintln!("Unsupported OS");
+            return;
+        }
+    };
+
+    let full_command = format!("{} {} {}", change_dir, server.executable, server.args.join(" "));
+
+    let status = Command::new(shell)
+        .args([shell_flag, &full_command])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn();
+
+    match status {
+        Ok(_) => println!("Started: {}", server.name),
+        Err(e) => eprintln!("Error starting {}: {}", server.name, e),
+    }
 }
