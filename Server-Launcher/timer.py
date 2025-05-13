aefa57b270ae4118362044b7508eb6a3281db42c
greg-chinnici <chinnici@chapman.edu>
2025-05-13 10:55:34 -0700

    python cmd line args improved

--- Server-Launcher/timer.py
+++ Server-Launcher/timer.py
@@ -4,14 +4,17 @@ import sys
 
 n = 10
 message = "python timer script"
+tick_timeout = 0.5
+
 
 for index , arg in enumerate(sys.argv):
     if index == 1:
         n = int(sys.argv[1])
     if index == 2:
         message = str(sys.argv[2])
+    if index == 3:
+        print(sys.argv[3])
 
-tick_timeout = 0.5
 print(f"Timer will go for {tick_timeout * n} seconds")
 
 for i in range(n):
